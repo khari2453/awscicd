ba981d17e356aa249310e316888f8570ec6f65b8
khari2453 <121078939+khari2453@users.noreply.github.com>
2025-09-23 19:27:57 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+#set -e
+
+#docker pull harikumar1997/simple-python-flask-app
+#echo
+
+#docker run -d -p 5000:5000 harikumar1997/simple-python-flask-app
+#echo
+#!/bin/bash
 set -e
+export PATH=$PATH:/usr/bin
 
-docker pull harikumar1997/simple-python-flask-app
-echo
+CONTAINER_NAME=simple-python-flask-app
+IMAGE_NAME=harikumar1997/simple-python-flask-app
 
-docker run -d -p 5000:5000 harikumar1997/simple-python-flask-app
-echo
+# Remove old container if exists
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    docker rm -f $CONTAINER_NAME
+fi
+
+echo "Starting container $CONTAINER_NAME..."
+docker run -d -p 5000:5000 --name $CONTAINER_NAME $IMAGE_NAME
